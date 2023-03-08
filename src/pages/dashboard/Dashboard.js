94005bc349f7888f918168ae5879c973ced309d1
BeengBeeng <nguyentienmanhtt1@gmail.com>
2023-03-08 18:13:13 +0700

    Featur/#411/updateprofile

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import Individual from '../individual/Individual'
-import PostUser from '../posts-user/PostUser'
-import UpdatePassword from '../update-password/UpdatePassword'
-import UpdateUser from '../update-user/UpdateUser'
+import React from 'react';
+import Individual from '../individual/Individual';
+import PostUser from '../posts-user/PostUser';
+import UpdatePassword from '../update-password/UpdatePassword';
+import UpdateUser from '../update-user/UpdateUser';
 
 const Dashboard = () => {
-  return (
-    <div>
-      {/* <Individual/> */}
-    {/* <UpdatePassword/> */}
-    <UpdateUser/>
-    {/* <PostUser/> */}
-    </div>
-  )
-}
-
+    return (
+        <div>
+            {/* <Individual/> */}
+            {/* <UpdatePassword/> */}
+            {/* <UpdateUser/> */}
+            <PostUser />
+        </div>
+    );
+};
 
 export default Dashboard;
